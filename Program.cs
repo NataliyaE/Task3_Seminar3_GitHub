e4aa74ef180050cad63319ad16a94b559a951fc3
Natali <salamatina_nata@mail.ru>
2022-09-12 17:18:39 +0300

    Task3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+GetDashboard(num);
+
+void GetDashboard (int num)
+{
+    int count = 1;
+    while (count <= Math.Abs(num))
+    {
+        Console.WriteLine($"куб числа {count} = {count*count*count}");
+        count++;
+    }
+}
+
